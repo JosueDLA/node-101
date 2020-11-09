e03e83c7eec5bade08ee871eba61fb307baa8d2d
JosueDLA <josuedlavs@gmail.com>
2020-11-09 09:29:39 -0600

    Add HTTP Module example

---
+++ http-module/app.js
@@ -0,0 +1,24 @@
+const http = require('http')
+
+// This server is an EventEmitter
+server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    // Handle different route
+    if (req.url === '/api/courses') {
+        res.write('Welcome to the API');
+        res.end();
+    }
+});
+
+// On Connection Event Listener
+server.on('connection', socket => {
+    console.log('New Connection...');
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
