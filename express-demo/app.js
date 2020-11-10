50bc606edb7a049c71c7101f6d61c492fa6b07bb
JosueDLA <josuedlavs@gmail.com>
2020-11-10 15:28:43 -0600

    Update port value

--- express-demo/app.js
+++ express-demo/app.js
@@ -9,5 +9,5 @@ app.get('/api/courses', (req, res) => {
     res.send('Welcome to the API...')
 });
 
-
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}}...`));
