3cb519698322f4e76fe1dd08c8fa56b82afb3943
JosueDLA <josuedlavs@gmail.com>
2020-11-10 16:42:12 -0600

    Add Route Parameters examples

--- express-demo/app.js
+++ express-demo/app.js
@@ -9,5 +9,20 @@ app.get('/api/courses', (req, res) => {
     res.send('Welcome to the API...')
 });
 
+// Route Parameters
+app.get('/api/courses/:id', (req, res) => {
+    res.send(`Course #${req.params.id} description...`);
+});
+
+// Multiple Routes Parameters
+app.get('/api/users/:first/:last', (req, res) => {
+    res.send(`Hola ${req.params.first} ${req.params.last}`);
+
+    // Query String Parameter
+    res.send(req.query);
+});
+
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
