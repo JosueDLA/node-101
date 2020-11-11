a4102863a21a5816f434876266bf33c5a8409e84
JosueDLA <josuedlavs@gmail.com>
2020-11-11 10:32:43 -0600

    Add CRUD operations on Express-demo

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,17 +1,104 @@
+const Joi = require('joi');
 const express = require('express');
+const { func } = require('joi');
+
 const app = express();
+
+app.use(express.json());
+
+// Dummy Data
+const courses = [
+    { id: 1, name: 'Course1' },
+    { id: 2, name: 'Course2' },
+    { id: 3, name: 'Course3' }
+];
+
+function validateCourse(course) {
+    // Course Schema
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    // Validate data with Joi
+    const result = schema.validate(course);
+
+    return result;
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send('Welcome to the API...')
+    res.send(courses)
 });
 
 // Route Parameters
 app.get('/api/courses/:id', (req, res) => {
-    res.send(`Course #${req.params.id} description...`);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    // Handling GET Requests
+    // Return 404 response
+    if (!course) {
+        return res.status(404).send('Course not found')
+    }
+
+    res.send(course);
+});
+
+// Handling POST Requests
+app.post('/api/courses', (req, res) => {
+
+    // const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        // Return 400 bad request
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+// Handling PUT Requests
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    // Return 404 response
+    if (!course) {
+        return res.status(404).send('Course not found')
+    }
+
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        // Return 400 bad request
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Update course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    // Return 404 response
+    if (!course) {
+        return res.status(404).send('Course not found')
+    }
+
+    // Delete course
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
 });
 
 // Multiple Routes Parameters
@@ -21,8 +108,6 @@ app.get('/api/users/:first/:last', (req, res) => {
     // Query String Parameter
     res.send(req.query);
 });
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
